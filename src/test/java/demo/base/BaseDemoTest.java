0e993fc956bbeaea84a3a04dbbfde0e60c0710f5
Noel Rollon <noel.rollon@oocl.com>
2022-08-26 11:57:59 +0800

    Add two cases in sum method

---
+++ src/test/java/demo/base/BaseDemoTest.java
@@ -0,0 +1,24 @@
+package demo.base;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BaseDemoTest {
+
+    @Test
+    public void should_return_hello_world() {
+        BaseDemo baseDemo = new BaseDemo();
+
+        String expected = "Hello World";
+
+        assertEquals(expected, baseDemo.print());
+    }
+
+    @Test
+    public void should_return_sum(){
+        BaseDemo baseDemo = new BaseDemo();
+        assertEquals(4, baseDemo.sum(2,2));
+        assertEquals(10, baseDemo.sum(5,5));
+    }
+}
